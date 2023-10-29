5f968bca099c515cafc60e5b03f7a2797a2735f4
Eric Ostrowski <eric@ericsco.de>
2023-10-28 23:09:24 -0500

    Added Lab5. Note that all wireframe pages will be MVC

---
+++ Lab5/Lab5/Controllers/PostController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab5.Controllers
+{
+    public class PostController : Controller
+    {
+        public IActionResult Details(int id)
+        {
+            // When a user clicks a post from the home page, this view will display that post
+            ViewBag.Message = $"This view will display details for post with ID: {id}";
+
+            return View();
+        }
+    }
+}
